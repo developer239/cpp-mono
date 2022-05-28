7a8f42dc06ee862da890029c0d403d9f87986572
michaljarnot <m.jarnot@yahoo.com>
2022-05-28 20:29:26 +0200

    feat: implement event bus

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,39 @@
-#include <thread>
 #include "core/Loop.h"
+#include "event-bus/EventBus.h"
 #include "core-sdl-implement/TimeImplement.h"
 #include "core-sdl-implement/RendererImplement.h"
+
+class KeyPressedEvent : public EventBase {
+  public:
+    std::string symbol;
+
+    explicit KeyPressedEvent(std::string& symbol) : symbol(symbol) {
+    }
+};
+
+class KeyboardControlSystem {
+  public:
+    void SubscribeToEvents(std::unique_ptr<EventBus>& eventBus) {
+      eventBus->SubscribeToEvent<KeyPressedEvent>(this, &KeyboardControlSystem::OnKeyPressed);
+    }
+
+    void OnKeyPressed(KeyPressedEvent& event) {
+      std::cout << "KeyboardControlSystem: " << event.symbol << " pressed" << std::endl;
+    }
+};
 
 int main() {
   auto time = std::make_shared<TimeImplement>();
   auto renderer = std::make_shared<RendererImplement>();
+
+  auto eventBus = std::make_unique<EventBus>();
+
+  auto keyboardControlSystem = KeyboardControlSystem();
+  keyboardControlSystem.SubscribeToEvents(eventBus);
+
+  std::string input = "some input";
+
+  eventBus->EmitEvent<KeyPressedEvent>(input);
 
   Loop loop(time, renderer);
   loop.Run();
