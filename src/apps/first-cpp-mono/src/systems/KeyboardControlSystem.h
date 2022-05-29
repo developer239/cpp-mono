99c0fb3dfe427518f18f04c3346b9a11c18fbc3b
michaljarnot <m.jarnot@yahoo.com>
2022-05-29 20:01:27 +0200

    feat: implement basic logic

---
+++ src/apps/first-cpp-mono/src/systems/KeyboardControlSystem.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <memory>
+#include "src/Bus.h"
+#include "src/Logger.h"
+#include "src/System.h"
+
+#include "../events/KeyPressedEvent.h"
+
+class KeyboardControlSystem : public System {
+  public:
+    void SubscribeToEvents(std::unique_ptr<Events::Bus>& eventBus) {
+      eventBus->SubscribeToEvent<KeyPressedEvent>(this, &KeyboardControlSystem::OnKeyPressed);
+    }
+
+    void OnKeyPressed(KeyPressedEvent& event) {
+      Logger::Log("KeyboardControlSystem: " + std::to_string(event.symbol) + " pressed");
+    }
+};
