3a36037c4ec7df308ba5d1611c0aa29a1990acef
michaljarnot <m.jarnot@yahoo.com>
2022-05-30 14:53:47 +0200

    refactor: extract core constructor

--- src/packages/loop/src/Core.cpp
+++ src/packages/loop/src/Core.cpp
@@ -1,5 +1,18 @@
 #include "Core.h"
 
+Loop::Core::Core(
+    std::shared_ptr<Tick> time, std::shared_ptr<Renderer> renderer, std::shared_ptr<EventManager> eventManager,
+    std::shared_ptr<IApp> app
+) :
+    time(std::move(time)),
+    eventManager(std::move(eventManager)) {
+  this->renderer = std::move(renderer);
+  this->renderer->state = &this->state;
+
+  this->app = std::move(app);
+  this->app->eventBus = this->eventManager->eventBus;
+}
+
 void Loop::Core::Setup() {
   renderer->Initialize(state.window.width, state.window.height);
   app->Setup();
